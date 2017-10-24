66b4c2857d8ab835e153453778ca6559cd5f78de
Ben Bergen <bergen@lanl.gov>
2017-10-24 08:18:44 -0600

    Modified walk logic to allow multiple docs.

--- ccli/ccli/services/doc.py
+++ ccli/ccli/services/doc.py
@@ -16,162 +16,154 @@ from ccli.services.doc_drivers.walk import *
 
 class CINCHDoc(Service):
 
-    #--------------------------------------------------------------------------#
-    # Initialization.
-    #--------------------------------------------------------------------------#
+  #--------------------------------------------------------------------------#
+  # Initialization.
+  #--------------------------------------------------------------------------#
 
-    def __init__(self, subparsers):
+  def __init__(self, subparsers):
 
-        """
-        """
+    """
+    """
 
-        # get a command-line parser
-        self.parser = subparsers.add_parser('doc',
-            help='Service to generate documentation using Pandoc.')
+    # get a command-line parser
+    self.parser = subparsers.add_parser('doc',
+      help='Service to generate documentation using Pandoc.')
 
-        # add command-line options
-        self.parser.add_argument('-c', '--config', action="store",
-            help='configuration module.' +
-                '  Load the configuration information' +
-                ' from python module CONFIG.')
+    # add command-line options
+    self.parser.add_argument('-c', '--config', action="store",
+      help='configuration module.' +
+        '  Load the configuration information' +
+        ' from python module CONFIG.')
 
-        self.parser.add_argument('-o', '--output', action="store",
-            help='output target.' +
-                '  Write output to file OUTPUT.')
+    self.parser.add_argument('-o', '--output', action="store",
+      help='output target.' +
+        '  Write output to file OUTPUT.')
 
-        self.parser.add_argument('-v', '--verbose', action="store_true",
-            help='verbose output.')
+    self.parser.add_argument('-v', '--verbose', action="store_true",
+      help='verbose output.')
 
-        self.parser.add_argument('-d', '--development', action="store_true",
-            help='development output (inline metadata).')
+    self.parser.add_argument('-d', '--development', action="store_true",
+      help='development output (inline metadata).')
 
-        self.parser.add_argument('directories', nargs='+',
-            help='A colon-delimited list of directories to process.')
+    self.parser.add_argument('directories', nargs='+',
+      help='A colon-delimited list of directories to process.')
 
-        # set the callback for this sub-command
-        self.parser.set_defaults(func=self.main)
+    # set the callback for this sub-command
+    self.parser.set_defaults(func=self.main)
 
-    # __init__
+  # __init__
 
-    #--------------------------------------------------------------------------#
-    # Main.
-    #--------------------------------------------------------------------------#
+  #--------------------------------------------------------------------------#
+  # Main.
+  #--------------------------------------------------------------------------#
 
-    def main(self, args=None):
+  def main(self, args=None):
 
-        """
-        """
+    """
+    """
 
-        # Recognized file suffixes
-        suffixes = (".md", ".tex")
+    # Recognized file suffixes
+    suffixes = (".md", ".tex")
 
-        # Setup default options
-        opts = {
-            'document' : 'Default',
-            'output' : 'cinchdoc.mdwn'
-        }
+    # Setup default options
+    opts = {
+      'document' : 'Default',
+      'output' : 'cinchdoc.mdwn'
+    }
 
-        #----------------------------------------------------------------------#
-        # Process command-line arguments
-        #----------------------------------------------------------------------#
+    #----------------------------------------------------------------------#
+    # Process command-line arguments
+    #----------------------------------------------------------------------#
 
-        # Check for user-defined configuration and import as module
-        # if option is set
-        if args.config:
-            (path, configfile) = os.path.split(args.config)
+    # Check for user-defined configuration and import as module
+    # if option is set
+    if args.config:
+      (path, configfile) = os.path.split(args.config)
 
-            # Add configfile path to module search path
-            if not path:
-                sys.path.append(os.getcwd())
-            else:
-                sys.path.append(path)
+      # Add configfile path to module search path
+      if not path:
+        sys.path.append(os.getcwd())
+      else:
+        sys.path.append(path)
 
-            # import the options from the specified module
-            opts = __import__(os.path.splitext(configfile)[0]).opts
-        # if
+      # import the options from the specified module
+      opts = __import__(os.path.splitext(configfile)[0]).opts
+    # if
 
-        # Check for command-line output specification
-        # NOTE: this option will over-write the value specified in
-        # the configuration file
-        if args.output:
-            opts['output'] = args.output
+    # Check for command-line output specification
+    # NOTE: this option will over-write the value specified in
+    # the configuration file
+    if args.output:
+      opts['output'] = args.output
 
-        #----------------------------------------------------------------------#
+    #----------------------------------------------------------------------#
 
-        # Create documents storage
-        documents = dict()
+    # Create the document object
+    document = Document(opts['document'])
 
-        # Create default document
-        documents['Default'] = Document('Default')
+    # Process sections-prepend option
+    if 'sections-prepend' in opts:
+      for section in opts['sections-prepend']:
+        document.add_section(section)
 
-        # Set default document
-        if not opts['document'] in documents:
-            documents[opts['document']] = Document(opts['document'])
-        doc = documents[opts['document']]
+    # Process sections option
+    if 'sections' in opts:
+      for section in opts['sections']:
+        document.add_section(section)
 
-        # Process sections-prepend option
-        if 'sections-prepend' in opts:
-            for chapter in opts['sections-prepend']:
-                doc.add_chapter(chapter)
+    # Recurse directories passed by user
+    for directory in args.directories:
 
-        # Process sections option
-        if 'sections' in opts:
-            for chapter in opts['sections']:
-                doc.add_chapter(chapter)
+      # Search sub-directories for documentation files
+      walk_tree(directory, suffixes, document,
+        args.verbose, args.development)
 
-        # Recurse directories passed by user
-        for directory in args.directories:
+    # Remove the default section if sections were found
+    # Because we add a 'header' section, there should be
+    # two sections if none were added by the user.
+    if (len(document.sections()) > 2) and 'Default' in document.sections():
+      document.delete_section('Default')
 
-            # Search sub-directories for documentation files
-            walk_tree(directory, suffixes, documents,
-                opts['document'], args.verbose, args.development)
+    #----------------------------------------------------------------------#
+    # Process sections-append
+    # This removes section-append keys and then re-adds them, this
+    # effectively sorts them to the end in-order because we are using
+    # an ordered dict.
+    #----------------------------------------------------------------------#
 
-        # Remove the default chapter if sections were found
-        # Because we add a 'header' chapter, there should be
-        # two sections if none were added by the user.
-        if (len(doc.sections()) > 2) and 'Default' in doc.sections():
-            doc.delete_chapter('Default')
+    if 'sections-append' in opts:
+      saved = dict()
+      for section in document.sections():
+        for key in opts['sections-append']:
+          if key == section:
+            saved[key] = document.sections()[key]
 
-        #----------------------------------------------------------------------#
-        # Process sections-append
-        # This removes chapter-append keys and then re-adds them, this
-        # effectively sorts them to the end in-order because we are using
-        # an ordered dict.
-        #----------------------------------------------------------------------#
+      for key in saved:
+        document.delete_section(key)
 
-        if 'sections-append' in opts:
-            saved = dict()
-            for chapter in doc.sections():
-                for key in opts['sections-append']:
-                    if key == chapter:
-                        saved[key] = doc.sections()[key]
+      for key in saved:
+        document.add_section(key, saved[key])
 
-            for key in saved:
-                doc.delete_chapter(key)
+    #----------------------------------------------------------------------#
+    # End Process sections-append
+    #----------------------------------------------------------------------#
 
-            for key in saved:
-                doc.add_chapter(key, saved[key])
+    # For now, just write to the specified output
+    document.write(opts['output'])
 
-        #----------------------------------------------------------------------#
-        # End Process sections-append
-        #----------------------------------------------------------------------#
+  # main
 
-        # For now, just write to the specified output
-        doc.write(opts['output'])
+  #--------------------------------------------------------------------------#
+  # Object factory for service creation.
+  #--------------------------------------------------------------------------#
 
-    # main
-
-    #--------------------------------------------------------------------------#
-    # Object factory for service creation.
-    #--------------------------------------------------------------------------#
-
-    class Factory:
-        def create(self, subparsers): return CINCHDoc(subparsers)
-    # class Factory
+  class Factory:
+    def create(self, subparsers): return CINCHDoc(subparsers)
+  # class Factory
 
 # class CINCHDoc
 
 #------------------------------------------------------------------------------#
 # Formatting options for emacs and vim.
-# vim: set tabstop=4 shiftwidth=4 expandtab :
+# vim: set tabstop=2 shiftwidth=2 expandtab :
 #------------------------------------------------------------------------------#
--- ccli/ccli/services/doc_drivers/utils.py
+++ ccli/ccli/services/doc_drivers/utils.py
@@ -7,93 +7,93 @@
 #------------------------------------------------------------------------------#
 
 def read_token(token, string):
-  offset = string.find(token) + len(token) + 1
-  value = string[offset]
-  while(string[offset+1] != ')'):
-    offset = offset+1
-    value = value + string[offset]
-  return value
+ offset = string.find(token) + len(token) + 1
+ value = string[offset]
+ while(string[offset+1] != ')'):
+  offset = offset+1
+  value = value + string[offset]
+ return value
 # read_token
 
 #------------------------------------------------------------------------------#
 #------------------------------------------------------------------------------#
 
 def read_tokens(line):
-  parsed = {}
-  for key in SYMBOLS:
-    if SYMBOLS[key] in line:
-      parsed[key] = read_token(SYMBOLS[key], line)
-    # if
-  # for
+ parsed = {}
+ for key in SYMBOLS:
+  if SYMBOLS[key] in line:
+   parsed[key] = read_token(SYMBOLS[key], line)
+  # if
+ # for
 
-  return parsed
+ return parsed
 # read_tokens
 
 special = [
-    '\'',
-    '"',
-    '`'
+  '\'',
+  '"',
+  '`'
 ]
 
 start_delimiters = [
-    '<!-- CINCHDOC',
-    '% CINCHDOC'
+  '<!-- CINCHDOC',
+  '% CINCHDOC'
 ]
 
 single_line_delimiters = [
-    '-->',
-    '% CINCHDOC'
+  '-->',
+  '% CINCHDOC'
 ]
 
 end_delimiters = [
-    '-->'
+  '-->'
 ]
 
 file_types = {
-    '.md' : 'markdown',
-    '.tex' : 'latex'
+  '.md' : 'markdown',
+  '.tex' : 'latex'
 }
 
 comment_start_delimiters = [
-    '<!--',
-    '%'
+  '<!--',
+  '%'
 ]
 
 def begin_identifier(line):
-    for char in special:
-        if char in line:
-            return False
+  for char in special:
+    if char in line:
+      return False
 
-    for delimiter in start_delimiters:
-        if delimiter in line:
-            return True
+  for delimiter in start_delimiters:
+    if delimiter in line:
+      return True
 # block_identifier
 
 def single_line_identifier(line):
-    for single in single_line_delimiters:
-        if single in line:
-            return True
+  for single in single_line_delimiters:
+    if single in line:
+      return True
 
-    return False
+  return False
 # single_line_identifier
 
 def end_identifier(line):
-    for end in end_delimiters:
-        if end in line:
-            return True
+  for end in end_delimiters:
+    if end in line:
+      return True
 
-    return False
+  return False
 # end_identifier
 
 def begin_comment_identifier(line):
-    for delimiter in comment_start_delimiters:
-        if line.startswith(delimiter):
-            return True
+  for delimiter in comment_start_delimiters:
+    if line.startswith(delimiter):
+      return True
 
-    return False
+  return False
 # begin_comment_identifier
 
 #------------------------------------------------------------------------------#
 # Formatting options for emacs and vim.
-# vim: set tabstop=4 shiftwidth=4 expandtab :
+# vim: set tabstop=2 shiftwidth=2 expandtab :
 #------------------------------------------------------------------------------#
